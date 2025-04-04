02aa840287f06583582d9e5329d2f8df8828a60b
Jbhoy2000 <120325768+Jbhoy2000@users.noreply.github.com>
2025-04-04 11:05:07 +0100

    wwise_files

---
+++ 2054/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID WEATHER = 317282339U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace WEATHER
+        {
+            static const AkUniqueID GROUP = 317282339U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INDOOR_RAIN = 3205863813U;
+                static const AkUniqueID INDOORS = 4103860031U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace WEATHER
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
