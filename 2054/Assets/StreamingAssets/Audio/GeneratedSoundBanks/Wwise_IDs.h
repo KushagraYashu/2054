5716bd0a2c0911ca27b4bf6f883c41238149661a
JOHN HICKEY <2302441@uad.ac.uk>
2025-04-15 16:32:47 +0100

    Audio_added

--- 2054/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ 2054/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,12 +17,15 @@ namespace AK
         static const AkUniqueID BACK_BUTTON = 2886972203U;
         static const AkUniqueID CHILDPLAYERFOOTSTEP = 3303107572U;
         static const AkUniqueID DOOR_OPEN = 535830432U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID START_BUTTON = 2674457354U;
         static const AkUniqueID TEENPLAYERFOOTSTEP = 450457492U;
         static const AkUniqueID TODDLERPLAYERFOOTSTEP = 969514050U;
+        static const AkUniqueID TV_STATIC = 1436086644U;
         static const AkUniqueID VOLCANO_EXPLOSION = 496050709U;
         static const AkUniqueID VOLCANO_HISS = 3420485851U;
         static const AkUniqueID WEATHER = 317282339U;
+        static const AkUniqueID WOOD_FLOOR_JUMP_LAND = 1900150648U;
     } // namespace EVENTS
 
     namespace STATES
@@ -40,6 +43,21 @@ namespace AK
         } // namespace WEATHER
 
     } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace TEEN_FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2775722229U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CARPET = 2412606308U;
+                static const AkUniqueID FLOOR = 1088209313U;
+            } // namespace SWITCH
+        } // namespace TEEN_FOOTSTEPS
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
